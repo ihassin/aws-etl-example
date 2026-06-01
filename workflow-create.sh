157f6b31d213d0c162c5de739d681c2f85d6ed60
Itamar Hassin <ihassin@gmail.com>
2026-06-01 16:47:47 -0400

    Extracted variables to the command line

--- workflow-create.sh
+++ workflow-create.sh
@@ -4,7 +4,17 @@ echo "Submitting stack"
 aws cloudformation deploy --template-file glue-example-workflow.yaml --stack-name glue-example-workflow --capabilities CAPABILITY_NAMED_IAM \
 --profile "$AWS_GLUE_EXAMPLE_PROFILE" --region "$AWS_GLUE_EXAMPLE_REGION" \
   --parameter-overrides \
-CityDataBucketName=$STAGING_BUCKET_NAME
+CityDataBucketName=$STAGING_BUCKET_NAME             \
+CityDataPath=city-data                              \
+CityDBName=city-data-db                             \
+CityTableName=city_data                             \
+CityDataProcessedTableName=city-data-processed      \
+ParquetPath=city_data.parquet                       \
+CityDataFileName=CityData.csv                       \
+CityDataETLScriptName=city_etl.py                   \
+CityDataDQScriptName=city_dq.py                     \
+CityDataProcessedPath=processed                     \
+ScriptPath=scripts
 
 echo "Loading scripts"
 aws s3 sync ./scripts/ "s3://$STAGING_BUCKET_NAME/scripts" \
