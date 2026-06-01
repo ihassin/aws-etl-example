5ce54f19bea35f594d88efc13fa579f1a442fbea
Itamar Hassin <ihassin@gmail.com>
2026-06-01 16:59:26 -0400

    Refactored script-path

--- workflow-create.sh
+++ workflow-create.sh
@@ -1,9 +1,11 @@
 #!/bin/zsh
 
 echo "Submitting stack"
+SCRIPT_PATH=scripts
+
 aws cloudformation deploy --template-file glue-example-workflow.yaml --stack-name glue-example-workflow --capabilities CAPABILITY_NAMED_IAM \
 --profile "$AWS_GLUE_EXAMPLE_PROFILE" --region "$AWS_GLUE_EXAMPLE_REGION" \
-  --parameter-overrides \
+--parameter-overrides \
 CityDataBucketName=$STAGING_BUCKET_NAME             \
 CityDataPath=city-data                              \
 CityDBName=city-data-db                             \
@@ -14,8 +16,8 @@ CityDataFileName=CityData.csv                       \
 CityDataETLScriptName=city_etl.py                   \
 CityDataDQScriptName=city_dq.py                     \
 CityDataProcessedPath=processed                     \
-ScriptPath=scripts
+ScriptPath="$SCRIPT_PATH"
 
-echo "Loading scripts"
-aws s3 sync ./scripts/ "s3://$STAGING_BUCKET_NAME/scripts" \
+echo "Loading scripts to $SCRIPT_PATH"
+aws s3 sync ./"$SCRIPT_PATH"/ "s3://$STAGING_BUCKET_NAME/$SCRIPT_PATH" \
 --profile "$AWS_GLUE_EXAMPLE_PROFILE" --region "$AWS_GLUE_EXAMPLE_REGION"
